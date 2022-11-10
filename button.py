bd7abf88eeadd56d121518fb79539e9fbc2c488a
LeahKearns <113065260+LeahKearns@users.noreply.github.com>
2022-11-10 13:55:25 -0800

    Update button.py

--- button.py
+++ button.py
@@ -1,4 +1,4 @@
-Python 3.10.3 (v3.10.3:a342a49189, Mar 16 2022, 09:34:18) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+
 Type "help", "copyright", "credits" or "license()" for more information.
 
 from graphics import *
