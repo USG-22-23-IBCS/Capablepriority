7fca71b5333f9b6df6a33495fc676043e4e07ded
Leah Kearns <leah_kearns@m018567.aw.org>
2023-02-03 14:17:27 -0800

    updated

--- bouncyball.py
+++ bouncyball.py
@@ -1,18 +1,39 @@
 from graphics import *
-
+from button import*
 import time
 
+def fall(C, direction):
+     height = 560 - C.getCenter().getY()
+     print(height)
+     time = 0
+     if direction == "down":
+         while C. getCenter().getY() < height +40:
+             time = time + .1
+             v = 9.8*time
+             C.move(0, v)
+             
+     if direction == "up":
+         while C. getCenter().getY() > 560 - height*10 - 40:
+            time = time + .1
+            v = 9.8*time
+            C.move(0, -v)
+
+def main():
+   
 # Initialize the window
-win = GraphWin("Bouncing Ball with Gravity", 800, 600)
+
+    win = GraphWin("Bouncing Ball with Gravity", 800, 600)
+    C = Circle(Point(400, 100), 40)
+    C.setFill("red")
+    C.draw(win)
+    win.setBackground("white")
+    fall(C, "down")
+    fall(C, "up")
 
 
-ball = Circle(Point(400, 100), 20)
-ball.setFill("red")
-ball.draw(win)
-win.setBackground("white")
 
-speed = 1
-gravity = 9.8
+    """"speed = 5
+    gravity = 9.8
 
 
 
@@ -27,8 +48,13 @@ while True:
     time.sleep(0.05)
 
 
-Text(Point(400, 300), "Bouncing Stopped").draw(win)
-win.getMouse()
-win.close()
+    Text(Point(400, 300), "Bouncing Stopped").draw(win)
+    win.getMouse()
+    win.close()"""
+
+if __name__ == "__main__":
+    main()
+    
+    
 
 
