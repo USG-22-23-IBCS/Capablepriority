7a73b74df21c3bc01075de33d393b24f63f5d287
Leah Kearns <leah_kearns@m018567.aw.org>
2023-03-02 14:06:44 -0800

    homework

---
+++ graphwork.py
@@ -0,0 +1,169 @@
+from button import *
+import random
+import time
+
+class Node:
+    def __init__(self, x, y, win, name):
+        self.center = Point(x, y)
+        self.C = Circle(self.center, 30)
+        self.neighbors = []
+        self.name = name
+        self.T = Text(self.center, self.name)
+
+    def draw(self, win):
+        self.C.draw(win)
+        self.T.draw(win)
+
+    def calcDegree(self):
+        return len(self.neighbors)
+
+    def getName(self):
+        return self.name
+
+    def undraw(self):
+        self.C.undraw()
+        self.T.undraw()
+
+    def addNeighbor(self, n):
+        self.neighbors.append(n)
+
+    def getCenter(self):
+        return self.center
+
+    def getNeighbors(self):
+        return self.neighbors
+
+    def color(self, c):
+        self.C.setFill(c)
+
+
+
+    def maxDegree(self):
+        
+        count_max = 0 
+        for n in self.getNeighbors():
+            deg = len(n.getNeighbors())
+            if deg > count_max:
+                count_max = deg 
+
+                
+        return count_max
+       
+  
+                   
+
+    def minDegree(self):
+        min_deg = 100
+        for n in self.getNeighbors():
+            deg = len(n.getNeighbors())
+            if deg < min_deg:
+                min_deg = deg
+                
+        return min_deg
+
+
+
+   
+
+class Graph:
+    def __init__(self, n, e, win):
+        self.nodes = []
+        self.E = []
+        Xpositions = []
+        Ypositions = []
+        names = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+        numN = 0
+        while True:
+            x = random.randint(140, 740)
+            y = random.randint(50, 550)
+            foundNode = True
+            for posX in Xpositions:
+                if x - 70 < posX < x + 70:
+                    for posY in Ypositions:
+                        if y - 70 < posY < y + 70:
+                            foundNode = False
+            if foundNode:
+                Xpositions.append(x)
+                Ypositions.append(y)
+                name = names[numN]
+                N = Node(x, y, win, name)
+                self.nodes.append(N)
+                numN += 1
+
+            if numN == n:
+                break
+
+        edges = 0
+        while edges < e:
+            n1 = random.choice(self.nodes)
+            n2 = random.choice(self.nodes)
+            if n1 != n2:
+                if n1 not in n2.getNeighbors():
+                    p1 = n1.getCenter()
+                    p2 = n2.getCenter()
+                    L = Line(p1, p2)
+                    self.E.append(L)
+                    L.draw(win)
+                    edges += 1
+                    n1.addNeighbor(n2)
+                    n2.addNeighbor(n1)
+
+        for node in self.nodes:
+            node.draw(win)
+            node.color("white")
+            print(str(node.calcDegree()) + " : " + node.getName())
+
+    def delete(self):
+        for e in self.E:
+            e.undraw()
+        for n in self.nodes:
+            n.undraw()
+        
+   
+
+    
+def main():
+ 
+    
+
+            
+    win = GraphWin("Graph Example", 800, 600)
+    Q = Button(win, Point(20, 530), Point(100, 590), "tomato", "QUIT!")
+    Gen = Button(win, Point(20, 430), Point(100, 490), "cyan", "Generate!")
+    G = Graph(1, 0, win)
+    while True:
+        m = win.getMouse()
+        if Q.isClicked(m):
+            break
+        if Gen.isClicked(m):
+            G.delete()
+            #GRaph made with number of nodes and number of edges
+            G = Graph(7, 7, win)
+
+            max_degree = 0
+            min_degree = 100
+            for node in G.nodes:
+                degree = node.calcDegree()
+                if degree > max_degree:
+                    max_degree = degree
+                if degree < min_degree:
+                    min_degree = degree
+
+            print("the max degree is:", max_degree)
+            print("the minimum degree is:", min_degree)
+    
+
+        
+    win.close()
+    
+            
+
+
+ 
+if __name__ == "__main__":
+    main()
+
+
+
+
+
